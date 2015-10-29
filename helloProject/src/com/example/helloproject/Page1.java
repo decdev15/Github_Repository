9bc2fd76f450dfeef5e2f22ae1265d71a457707d
Declan Devine <decdev830@gmail.com>
2015-10-29 16:42:36 +0000

    Geog project

---
+++ helloProject/src/com/example/helloproject/Page1.java
@@ -0,0 +1,14 @@
+package com.example.helloproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Page1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.page1);
+	}
+	
+}
